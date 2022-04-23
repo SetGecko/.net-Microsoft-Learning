56bdec1f2c95eaad97615c521273382a25ed6813
SetGecko <81488631+SetGecko@users.noreply.github.com>
2022-04-23 20:38:20 +0300

    upload literals and variables

---
+++ 2_literals.cs
@@ -0,0 +1,12 @@
+// Литералы
+// Вывод литерала в одинарных кавычках - char
+Console.WriteLine('b');
+// Вывод длинного стринга в двойных кавычках - string
+Console.WriteLine("Hello World!");
+// Вывод целочичисленного литерала делается без кавычек - int
+Console.WriteLine(123);
+// Десятичный литерал с помощью литерального суффикса m или M - decimal
+Console.WriteLine(12.3m);
+// Вывод логических литералов (булевых - bool)
+Console.WriteLine(true);
+Console.WriteLine(false);
---
+++ 3_variables.cs
@@ -0,0 +1,35 @@
+// string firstName;
+// char userOption;
+// int gameScore;
+// decimal particlesPerMillion;
+// bool processedCustomer;
+// string firstName;
+// firstName = "Bob";
+// Console.WriteLine(firstName);
+// firstName = "Beth";
+// Console.WriteLine(firstName);
+// firstName = "Conrad";
+// Console.WriteLine(firstName);
+// firstName = "Grant";
+// Console.WriteLine(firstName);
+
+// string firstName = "Bob";
+// Console.WriteLine(firstName);
+
+// var message = "Hello world!";
+
+
+// Необходимо сделать вывод на экран: Hello, Bob! You have 3 in your inbox. The temperature is 34.4 celsius.
+// где присутствую следующие переменные: Bob, 3, 34.4
+
+string name = "Bob";
+int lettersInbox = 3;
+decimal temperature = 34.4m;
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(lettersInbox);
+Console.Write(" in your inbox. ");
+Console.Write("The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
