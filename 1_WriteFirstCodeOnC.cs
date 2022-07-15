9e35113b9d966d9e07c6f66da79f9d91a55dcc80
SetGecko <81488631+SetGecko@users.noreply.github.com>
2022-07-15 22:08:51 +0300

    Create 1_WriteFirstCodeOnC.cs

---
+++ 1_WriteFirstCodeOnC.cs
@@ -0,0 +1,10 @@
+// Это комментарий
+Console.WriteLine("Hello, World!");
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
+Console.WriteLine("");
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
